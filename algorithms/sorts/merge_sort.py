5929b65d2964b5b4d06405e77a18fd8582cdd62d
Chake96 <Carsonhake@gmail.com>
2020-08-30 11:03:45 -0400

    added another implementation of merge_sort
    1)merge_sort.py now has two implementations
    	a)a single function implementation
    	b)a function with helper function implementation -> faster version of merge_sort

--- algorithms/sorts/merge_sort.py
+++ algorithms/sorts/merge_sort.py
@@ -1,13 +1,16 @@
+# A divide and conquer algorithm
 # Time Complexity:
-#Best Case: O(nlogn)
-#Worst Case: O(nlogn)
-#Avg Case: O(nlogn)
+#	Best Case: O(nlogn)
+#	Worst Case: O(nlogn)
+#	Avg Case: O(nlogn)
 # Space Complexity: O(n)
+# Single Function Implementation vs Function with Helper
+	# helper function is the faster implementation 
 #---------------------------------------------------------------
 from random import randint
 import sys
 import datetime
-
+#python3 single function merge_sort implementation
 def merge_sort(arr):
 	if len(arr) > 1:
 		mid = len(arr)//2
@@ -33,8 +36,36 @@ def merge_sort(arr):
 			arr[k] = right[j]
 			j += 1
 			k += 1
-		
-
+#merge sort with a helper function
+def merge_sort2(arr, l=0, r=None):
+	if r is None: 
+		r = len(arr)
+	if l < r-1:
+		mid = (l+r)//2
+		merge_sort2(arr, l,mid)
+		merge_sort2(arr, mid,r)
+		merge(arr, l, mid, r)
+#helper function to merge the arrays/lists from merge_sort2
+def merge(arr, l, mid, r):
+	result, i, j = [], l, mid
+	while True:
+		if arr[i] <= arr[j]:
+			result.append(arr[i])
+			i += 1
+		else:
+			result.append(arr[j])
+			j += 1
+		if i == mid:
+			while(j < r):
+				result.append(arr[j])
+				j+=1
+			break
+		if j == r:
+			while(i < mid):
+				result.append(arr[i])
+				i+=1
+			break;
+	arr[l:r] = result
 if __name__ == "__main__":
 	if len(sys.argv) >2:
 		a = [randint(0,int(sys.argv[2])) for k in range(int(sys.argv[1]))]
@@ -46,13 +77,21 @@ if __name__ == "__main__":
 		a = [randint(0,100) for k in range(20)]
 		print("Sorting 20 random numbers in range [0,100]")
 	start = datetime.datetime.now()
-	merge_sort(a)
+	b = a
+	merge_sort(b)
 	end = datetime.datetime.now()
 	elapsed = end - start
 	microseconds = elapsed.microseconds
 	total_seconds = elapsed.total_seconds()
 	minutes = (total_seconds //60)%60
 	seconds = (total_seconds %60)
-	print("Time elapsed(min:sec) = %d:%d \nTotal Seconds: %d \nTotal microseconds: %d " %(minutes, seconds, total_seconds, microseconds))
-	if len(a) > 10000:
-		print(a[-5:])
+	print("Single Function Time elapsed(min:sec) = %d:%d \nTotal Seconds: %d \nTotal microseconds: %d " %(minutes, seconds, total_seconds, microseconds))
+	start = datetime.datetime.now()
+	merge_sort2(a)
+	end = datetime.datetime.now()
+	elapsed = end - start
+	microseconds = elapsed.microseconds
+	total_seconds = elapsed.total_seconds()
+	minutes = (total_seconds //60)%60
+	seconds = (total_seconds %60)
+	print("Function with Helper Time elapsed(min:sec) = %d:%d \nTotal Seconds: %d \nTotal microseconds: %d " %(minutes, seconds, total_seconds, microseconds))
